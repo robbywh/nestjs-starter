f3d005194ae285a92ee57816f2e5bed28d50b643
Robby Widyahartono <robby.widyahartono@komunal.co.id>
2023-12-01 16:40:57 +0700

    integrate with redis

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,14 @@
-import { Injectable, LoggerService } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable, LoggerService } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AppService {
   constructor(
     private readonly configService: ConfigService,
     private readonly logger: LoggerService,
+    @Inject(CACHE_MANAGER) private readonly cache: Cache,
   ) {}
   getHello(): string {
     this.logger.log('Hello World!', 'AppService');
