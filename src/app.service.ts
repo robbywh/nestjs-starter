18353874c8dc62e7f371e9c64c7d301998c79934
Robby Widyahartono <robby.widyahartono@komunal.co.id>
2023-12-03 21:58:52 +0700

    update test

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,19 +1,8 @@
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Inject, Injectable, LoggerService } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { Cache } from 'cache-manager';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly configService: ConfigService,
-    private readonly logger: LoggerService,
-    @Inject(CACHE_MANAGER) private readonly cache: Cache,
-  ) {}
   getHello(): string {
-    this.logger.log('Hello World!', 'AppService');
-    const configValue = this.configService.get(`environment`);
-    console.log(`getting value from config: ${configValue}`);
     return 'Hello World!';
   }
 }
